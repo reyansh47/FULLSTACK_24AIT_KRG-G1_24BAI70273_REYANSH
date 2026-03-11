4a77f8e9ce11ea6153e86fbac12fcda1dadb35e2
Reyansh Arora <reyansharora58@gmail.com>
2026-03-11 23:28:08 +0530

    Update script.js

--- LABMST/script.js
+++ LABMST/script.js
@@ -1,1 +1,18 @@
 
+console.log("Waiting for data...");
+
+var myPromise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve("Data fetched successfully");
+    }, 2000);
+});
+
+myPromise.then(function (message) {
+    console.log(message);
+}).catch(function (error) {
+    console.log(error); 
+});  
+
+
+
+
